5597f92059e14690184c60d13ac650a1e1ae0c1e
vacponto <62482447+zcaxs@users.noreply.github.com>
2025-10-01 16:53:47 +0100

    Finished Audio Menu UI

--- ABF/Assets/Scripts/VolumeSelector.cs
+++ ABF/Assets/Scripts/VolumeSelector.cs
@@ -1,12 +1,15 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 
 public class VolumeSelector : MonoBehaviour
 {
-    public TMP_Text volumeText; 
-    public int volume = 5;  
+    public TMP_Text volumeText;
+    public Button leftArrowButton; // Drag your left arrow button here in inspector
+    public Button rightArrowButton; // Drag your right arrow button here in inspector
+    public Button targetButton;
+    public int volume = 5;
     public int minVolume = 0;
     public int maxVolume = 10;
 
@@ -14,29 +17,49 @@ public class VolumeSelector : MonoBehaviour
     {
         UpdateVolumeText();
         AudioListener.volume = volume / 10f;
+
+        // Add click listeners to buttons
+        if (leftArrowButton != null)
+            leftArrowButton.onClick.AddListener(DecreaseVolume);
+        if (rightArrowButton != null)
+            rightArrowButton.onClick.AddListener(IncreaseVolume);
     }
 
     void Update()
     {
-        if (EventSystem.current.currentSelectedGameObject == volumeText.gameObject)
+        if (EventSystem.current.currentSelectedGameObject == targetButton.gameObject)
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                volume = Mathf.Max(minVolume, volume - 1);
-                UpdateVolumeText();
-            }
-            if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                volume = Mathf.Min(maxVolume, volume + 1);
-                UpdateVolumeText();
-            }
-        }
+            if (Input.GetKeyDown(KeyCode.A))
+                DecreaseVolume();
 
+            if (Input.GetKeyDown(KeyCode.D))
+                IncreaseVolume();
+        }
+    }
+
+    public void DecreaseVolume()
+    {
+        if (volume > minVolume)
+        {
+            volume--;
+            UpdateVolumeText();
+            Debug.Log($"✅ Volume decreased to: {volume}");
+        }
+    }
+
+    public void IncreaseVolume()
+    {
+        if (volume < maxVolume)
+        {
+            volume++;
+            UpdateVolumeText();
+            Debug.Log($"✅ Volume increased to: {volume}");
+        }
     }
 
     void UpdateVolumeText()
     {
         volumeText.text = volume.ToString();
-        AudioListener.volume = volume / 10f; 
+        AudioListener.volume = volume / 10f;
     }
 }
