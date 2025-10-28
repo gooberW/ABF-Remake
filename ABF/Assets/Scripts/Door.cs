716a4a5096e0049da3271370fe51801dcf075f4b
pedroorodrigues <jose_rodrigues@eticalgarve.com>
2025-10-28 11:58:13 +0000

    door fix

--- ABF/Assets/Scripts/Door.cs
+++ ABF/Assets/Scripts/Door.cs
@@ -1,51 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour, Idoorincteractable
+public class Door : MonoBehaviour
 {
-    
-    private HingeJoint joint;
-    private Transform T;
-    private bool isDoorOpen;
+    [Header("Configurações da Porta")]
+    public float openAngle = 90f;           
+    public float openSpeed = 120f;          
+    public bool invertDirection = false;   
 
-    public bool IsDoorOpen { get => isDoorOpen; set => isDoorOpen = value; }
+    [Header("Interação")]
+    [SerializeField] private Camera cam;
+    [SerializeField] private LayerMask doorLayer;
+    [SerializeField] private CrosshairController crosshairController;
+    [SerializeField] private string openPrompt = "Open Door";
+    [SerializeField] private string closePrompt = "Close Door";
 
-    private void Start()
+    private Transform selectedDoor;
+    private bool isLookingAtDoor = false;
+    private bool isDoorOpen = false;
+    public bool IsDoorOpen => isDoorOpen;
+
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+    private bool isMoving = false;
+
+    void Start()
     {
-        joint = GetComponent<HingeJoint>();
+        closedRotation = transform.localRotation;
+
+        
+        float direction = invertDirection ? -1f : 1f;
+        openRotation = closedRotation * Quaternion.Euler(0f, openAngle * direction, 0f);
     }
 
-    public void OnDoorInteratable()
+    void Update()
     {
-        HingeJoint joint = T.GetComponent<HingeJoint>();
-        JointMotor motor = joint.motor;
+        RaycastHit hit;
+        bool hitDoor = Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 2.5f, doorLayer);
 
+        if (hitDoor)
+        {
+            if (!isLookingAtDoor)
+            {
+                crosshairController.SetInteractable(isDoorOpen ? closePrompt : openPrompt);
+                isLookingAtDoor = true;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                ToggleDoor();
+            }
+        }
+        else if (isLookingAtDoor)
+        {
+            crosshairController.SetNormal();
+            isLookingAtDoor = false;
+        }
+
+        if (isMoving)
+        {
+            MoveDoor();
+        }
+    }
+
+    private void MoveDoor()
+    {
+        Quaternion targetRot = isDoorOpen ? openRotation : closedRotation;
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, targetRot, openSpeed * Time.deltaTime);
+
+        if (Quaternion.Angle(transform.localRotation, targetRot) < 0.5f)
+        {
+            transform.localRotation = targetRot;
+            isMoving = false;
+        }
+    }
+
+    public void ToggleDoor()
+    {
+        if (isMoving) return; 
         isDoorOpen = !isDoorOpen;
+        isMoving = true;
 
-        float speedMultiplier = 60000;
-
-        int leftDoor = T.GetComponent<MeshRenderer>().localBounds.center.x > T.localPosition.x ? 1 : -1;
-
-        if (isDoorOpen)
-        {
-
-            motor.targetVelocity = speedMultiplier * Time.deltaTime * leftDoor;
-
-        }
-        if (!isDoorOpen)
-        {
-
-            motor.targetVelocity = -speedMultiplier * Time.deltaTime * leftDoor;
-
-        }
-
-        joint.motor = motor;
-
-
-        //if (isLookingAtDoor)
-        //{
-        //    crosshairController.SetInteractable(isDoorOpen ? closePrompt : openPrompt);
-        //}
+        crosshairController.SetInteractable(isDoorOpen ? closePrompt : openPrompt);
     }
 }
--- ABF/Assets/Scripts/PlayerScript.cs
+++ ABF/Assets/Scripts/PlayerScript.cs
@@ -40,15 +40,9 @@ public class PlayerScript : MonoBehaviour
     //[SerializeField] private NotebookScript _notebook;
     [SerializeField] private CanvasGroup _staminaBar;
     [SerializeField] private UnityEngine.UI.Image _slider;
-    [SerializeField] Camera cam;
-    [SerializeField] private LayerMask doorLayer;
-    [SerializeField] CrosshairController crosshairController;
-    [SerializeField] string openPrompt = "Open Door";
-    [SerializeField] string closePrompt = "Close Door";
-    [SerializeField] private bool isDoorOpen = false;
-    Transform selectedDoor;
+
     private bool isLookingAtDoor = false;
-    Door Door;
+    
 
     void Start()
     {
@@ -124,33 +118,9 @@ public class PlayerScript : MonoBehaviour
         CheckStamina();
         //Debug.Log(_currentStamina);
 
-        RaycastHit hit;
-        bool hitDoor = Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 20, doorLayer);
-        if (hitDoor && !isLookingAtDoor)
-        {
-            Door = hit.collider.GetComponent<Door>();
-            if (selectedDoor != null)
-            {
-                crosshairController.SetInteractable( Door.IsDoorOpen ? closePrompt : openPrompt);
-            }
-            else
-            {
-                crosshairController.SetInteractable(openPrompt);
-            }
-            isLookingAtDoor = true;
-        }
-        else if (!hitDoor && isLookingAtDoor)
-        {
-            crosshairController.SetNormal();
-            isLookingAtDoor = false;
-        }
-
-        if (hitDoor && Input.GetMouseButtonDown(0))
-        {
-            selectedDoor = hit.collider.gameObject.transform;
-            Door.OnDoorInteratable();
-        }
+        
     }
+
 
     void CheckStamina()
     {
